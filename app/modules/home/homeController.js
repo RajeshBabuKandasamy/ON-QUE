499a4244017cfe82aeaaebbf0675a1650be4d27e
Rajesh Babu <rajeshbabu@arivuventures.com>
2018-01-21 14:10:03 +0530

    home page updated code

--- app/modules/home/homeController.js
+++ app/modules/home/homeController.js
@@ -5,30 +5,48 @@ OnqueApp.controller('homeController', [
   function ($scope, $window, DoctorsList) {	
   $scope.searchText = "";
   $scope.searchSection = false;
+  $scope.searchInputSection = true;
   $scope.doctor = [];
-
+  
   angular.element($window).bind("scroll", function() {
       $scope.position = $window.scrollY;
-      console.log($scope.position);
+      $scope.windowWidth = $window.innerWidth;
       var myEl = angular.element(document.querySelector('.myHeadercss'));
-      if ($scope.position >= 330) {
+      if($scope.windowWidth <= 768){
+        $scope.onqueheaderConfig = {
+             showSearch: false
+       }
+       $scope.searchInputSection = true;
+       if($scope.searchText != ''){
+          $scope.searchSection = true;
+        }
+      }
+      else{
+      if ($scope.position >= 295) {
        $scope.onqueheaderConfig = {
         hambergFlag: true,
         showSearch: true
         }
+     $scope.searchInputSection = false;
+     $scope.searchSection = false;
      myEl.css({'background-color':'#172636', 'opacity':'1'});
 	   }
-   else if ($scope.position <= 330) {
+   else if ($scope.position <= 295) {
       	$scope.onqueheaderConfig = {
           hambergFlag: true,
           showSearch: false
         }
+      $scope.searchInputSection = true;
+      if($scope.searchText != ''){
+          $scope.searchSection = true;
+      }
         myEl.css({'background-color':'transparent', 'opacity':'1'});
     }
     $scope.$apply();
-  });
+   }
+});
 
-  $scope.searchKeyDownEvent = function($event){
+$scope.searchKeyDownEvent = function($event){
         if($scope.searchText == ''){
           $scope.searchSection = false;
         }else{
@@ -40,9 +58,7 @@ OnqueApp.controller('homeController', [
   	var doctorsCollection = DoctorsList.result($scope.searchText);
     	   doctorsCollection.success(function(data){
           $scope.searchSection = true;
-      		$scope.doctor = data;
-    	    console.log($scope.doctor);
-    	  
+      		$scope.doctor = data; 
     	}).error(function(err){
     	    	            });
       }
@@ -52,6 +68,5 @@ OnqueApp.controller('homeController', [
         hambergFlag: true,
         showSearch: false
     }
-
   
 }]);
